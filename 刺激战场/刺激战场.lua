ba2f3b0030b890526b640c0573d6c24e3c4867d9
kingsollyu <191067617@qq.com>
2018-03-20 01:10:37 +0800

    增加2、4、8倍镜
      * 对应版本：0.4.0.3200
      * 修正UMP9、AKM、M416\SCAR-L 红点、2倍镜数据

--- 刺激战场/刺激战场.lua
+++ 刺激战场/刺激战场.lua
@@ -14,10 +14,10 @@ pubg.keyAlwayRun     = 4
 -- 数据定义区域
 -------------------------------------------------------------------------------
 pubg.data = {
-	UMP9  = {RELATIVE=1, SLEEP=20},
-	UZI   = {RELATIVE=1, SLEEP=20},
-	M416  = {RELATIVE=1, SLEEP=20},
-	AKM   = {RELATIVE=1, SLEEP=20},
+	UMP9  = {RELATIVE_X1=2, SLEEP_X1=80, RELATIVE_X2=2, SLEEP_X2=47, RELATIVE_X4=5, SLEEP_X4=15, RELATIVE_X8=2, SLEEP_X8=47},
+	UZI   = {RELATIVE_X1=2, SLEEP_X1=20, RELATIVE_X2=2, SLEEP_X2=80, RELATIVE_X4=2, SLEEP_X4=47, RELATIVE_X8=2, SLEEP_X8=47},
+	M416  = {RELATIVE_X1=2, SLEEP_X1=70, RELATIVE_X2=2, SLEEP_X2=45, RELATIVE_X4=2, SLEEP_X4=47, RELATIVE_X8=2, SLEEP_X8=47},
+	AKM   = {RELATIVE_X1=1, SLEEP_X1=25, RELATIVE_X2=1, SLEEP_X2=14, RELATIVE_X4=2, SLEEP_X4=47, RELATIVE_X8=2, SLEEP_X8=47},
 }
 
 -------------------------------------------------------------------------------
@@ -34,6 +34,11 @@ pubg.currentWeapon = "NIL"
 -- pubg逻辑处理
 -------------------------------------------------------------------------------
 pubg.onEvent = function (event, arg)
+
+	if event == "PROFILE_ACTIVATED" then
+		EnablePrimaryMouseButtonEvents(true)
+	end
+
 	-- G键按下逻辑
 	if event == "MOUSE_BUTTON_PRESSED" and arg == 6 then
 		pubg.isGKeyPressed = true
@@ -50,18 +55,24 @@ pubg.onEvent = function (event, arg)
 	-- 当鼠标有G键被松开时，如果其他没有其他按键按下，则说明为单点G键，关闭压抢
 	if event == "MOUSE_BUTTON_RELEASED" and arg == 6 and pubg.isOKeyPressed == false then
 		pubg.currentWeapon = "NIL"
+		OutputLogMessage("shutdown\n")
 	end
 
 	-- 鼠标按键逻辑区域
 	if event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyWeaponUmp9 and pubg.isGKeyPressed == true then
+		OutputLogMessage("use UMP9\n")
 		pubg.currentWeapon = "UMP9"
 	elseif event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyWeaponUzi and pubg.isGKeyPressed == false then
+		OutputLogMessage("use UZI\n")
 		pubg.currentWeapon = "UZI"
 	elseif event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyWeaponAkm and pubg.isGKeyPressed == true then
+		OutputLogMessage("use AKM\n")
 		pubg.currentWeapon = "AKM"
 	elseif event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyWeaponM416 and pubg.isGKeyPressed == false then
+		OutputLogMessage("use M416\n")
 		pubg.currentWeapon = "M416"
 	elseif event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyAlwayRun and pubg.isGKeyPressed == false then
+		OutputLogMessage("use AlwayRun\n")
 		PressKey(0x0d)
 		Sleep(400)
 		ReleaseKey(0x0d)
@@ -70,8 +81,18 @@ pubg.onEvent = function (event, arg)
 	-- 鼠标左键被按下
 	if event == "MOUSE_BUTTON_PRESSED" and arg == 1 and pubg.currentWeapon ~= "NIL" then
 		repeat
-			MoveMouseRelative(0, pubg.data[pubg.currentWeapon]["RELATIVE"])
-			Sleep(pubg.data[pubg.currentWeapon]["SLEEP"])
+			if IsKeyLockOn ("capslock") then
+				if IsModifierPressed ("lshift") then
+					MoveMouseRelative(0, pubg.data[pubg.currentWeapon]["RELATIVE_X4"])
+					Sleep(pubg.data[pubg.currentWeapon]["SLEEP_X4"])
+				else
+					MoveMouseRelative(0, pubg.data[pubg.currentWeapon]["RELATIVE_X2"])
+					Sleep(pubg.data[pubg.currentWeapon]["SLEEP_X2"])
+				end
+			else
+				MoveMouseRelative(0, pubg.data[pubg.currentWeapon]["RELATIVE_X1"])
+				Sleep(pubg.data[pubg.currentWeapon]["SLEEP_X1"])
+			end
 		until not IsMouseButtonPressed(1)
 	end
 end
