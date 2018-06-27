122733c0f60bb59fcbb1d7795758d212a96d040c
sollyu <sollyu@qq.com>
2018-06-27 16:05:58 +0800

    -

--- 刺激战场/刺激战场.lua
+++ 刺激战场/刺激战场.lua
@@ -1,26 +1,6 @@
 local pubg = {}
 
 -------------------------------------------------------------------------------
--- 快捷键定义区域
--------------------------------------------------------------------------------
-pubg.keyWeaponUmp9   = 8
-pubg.keyWeaponAkm    = 7
-pubg.keyWeaponM16a4  = 9
-pubg.keyWeaponUzi    = 7
-pubg.keyWeaponM416   = 5
-pubg.keyAlwayRun     = 4
-
--------------------------------------------------------------------------------
--- 数据定义区域
--------------------------------------------------------------------------------
-pubg.data = {
-	UMP9  = {RELATIVE_X1=2, SLEEP_X1=80, RELATIVE_X2=2, SLEEP_X2=47, RELATIVE_X4=5, SLEEP_X4=15, RELATIVE_X8=2, SLEEP_X8=47},
-	UZI   = {RELATIVE_X1=2, SLEEP_X1=20, RELATIVE_X2=2, SLEEP_X2=80, RELATIVE_X4=2, SLEEP_X4=47, RELATIVE_X8=2, SLEEP_X8=47},
-	M416  = {RELATIVE_X1=2, SLEEP_X1=70, RELATIVE_X2=2, SLEEP_X2=45, RELATIVE_X4=2, SLEEP_X4=47, RELATIVE_X8=2, SLEEP_X8=47},
-	AKM   = {RELATIVE_X1=1, SLEEP_X1=25, RELATIVE_X2=1, SLEEP_X2=14, RELATIVE_X4=2, SLEEP_X4=47, RELATIVE_X8=2, SLEEP_X8=47},
-}
-
--------------------------------------------------------------------------------
 -- 变量定义区域
 -------------------------------------------------------------------------------
 -- 鼠标G键是否被按下
@@ -31,12 +11,138 @@ pubg.isOKeyPressed = false
 pubg.currentWeapon = "NIL"
 
 -------------------------------------------------------------------------------
+-- 压枪逻辑
+-------------------------------------------------------------------------------
+-- UMP9 
+pubg.onWeaponUMP9_X1 = function()
+end
+
+pubg.onWeaponUMP9_X2 = function()
+end
+
+pubg.onWeaponUMP9 = function()
+	ClearLog()
+	-- 一倍压枪
+	local weaponX1 = function() 
+		local shotTimes = 0
+		repeat
+			MoveMouseRelative(0, 1)
+			if shotTimes < 25 then
+				Sleep(30)
+			elseif shotTimes < 70 then
+				Sleep(25)
+			else
+				Sleep(23)
+			end
+				
+			shotTimes = shotTimes + 1
+		until not IsMouseButtonPressed(1)
+		
+		if shotTimes > 60 then
+			pubg.reloadWeapon()
+		end
+	end
+	
+	local weaponX2 = function()
+		local shotTimes = 0
+		repeat
+			MoveMouseRelative(0, 1)
+			shotTimes = shotTimes + 1
+			Sleep(19)
+		until not IsMouseButtonPressed(1)
+		
+		if shotTimes > 140 then
+			pubg.reloadWeapon()
+		end
+	end
+	
+	if IsModifierPressed("shift") then
+		weaponX2()
+	else
+		weaponX1()
+	end
+end
+
+pubg.onWeaponScarL = function()
+	
+	-------------------------------------------
+	-- X1
+	-------------------------------------------
+	local weaponX1 = function()
+		local shotTimes = 0
+		repeat
+			MoveMouseRelative(0, 1)
+			shotTimes = shotTimes + 1
+			if shotTimes < 40 then
+				Sleep(25)
+			else
+				Sleep(20)
+			end
+		until not IsMouseButtonPressed(1)
+	end
+
+	-------------------------------------------
+	-- X2
+	-------------------------------------------
+	local weaponX2 = function()
+		local shotTimes = 0
+		repeat
+			MoveMouseRelative(0, 1)
+			shotTimes = shotTimes + 1
+			if shotTimes < 20 then
+				Sleep(25)
+			else
+				Sleep(15)
+			end
+		until not IsMouseButtonPressed(1)
+		if shotTimes > 120 then
+			pubg.reloadWeapon()
+		end
+	end
+	
+	-------------------------------------------
+	-- X4
+	-------------------------------------------
+	local weaponX4 = function()
+		local shotTimes = 0
+		repeat
+			Sleep(50)
+			MoveMouseRelative(0, 5)
+			shotTimes = shotTimes + 1			
+			if shotTimes > 3 then
+				ReleaseMouseButton(1)
+			end
+		until not IsMouseButtonPressed(1)
+	end
+
+
+	if IsKeyLockOn("capslock") then
+		weaponX4()
+	elseif IsModifierPressed("shift") then
+		weaponX2()
+	else
+		weaponX1()
+	end	
+
+end
+
+pubg.reloadWeapon = function()
+	PressKey(0x13)
+	Sleep(200)
+	ReleaseKey(0x13)
+end
+
+-------------------------------------------------------------------------------
 -- pubg逻辑处理
 -------------------------------------------------------------------------------
 pubg.onEvent = function (event, arg)
-
+	--OutputLogMessage("event:" .. tostring(event) .. " arg:" .. tostring(arg) .. " \n")
+	
 	if event == "PROFILE_ACTIVATED" then
 		EnablePrimaryMouseButtonEvents(true)
+	elseif event == "PROFILE_DEACTIVATED" then
+		EnablePrimaryMouseButtonEvents(false)
+		return
 	end
 
 	-- G键按下逻辑
@@ -55,47 +161,29 @@ pubg.onEvent = function (event, arg)
 	-- 当鼠标有G键被松开时，如果其他没有其他按键按下，则说明为单点G键，关闭压抢
 	if event == "MOUSE_BUTTON_RELEASED" and arg == 6 and pubg.isOKeyPressed == false then
 		pubg.currentWeapon = "NIL"
-		OutputLogMessage("shutdown\n")
+		OutputLogMessage("shutdown anti recoil\n")
+		return
 	end
 
-	-- 鼠标按键逻辑区域
-	if event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyWeaponUmp9 and pubg.isGKeyPressed == true then
-		OutputLogMessage("use UMP9\n")
+	-- 武器选择判定
+	if event == "MOUSE_BUTTON_RELEASED" and arg == 8 and pubg.isOKeyPressed == false then
 		pubg.currentWeapon = "UMP9"
-	elseif event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyWeaponUzi and pubg.isGKeyPressed == false then
-		OutputLogMessage("use UZI\n")
-		pubg.currentWeapon = "UZI"
-	elseif event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyWeaponAkm and pubg.isGKeyPressed == true then
-		OutputLogMessage("use AKM\n")
-		pubg.currentWeapon = "AKM"
-	elseif event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyWeaponM416 and pubg.isGKeyPressed == false then
-		OutputLogMessage("use M416\n")
-		pubg.currentWeapon = "M416"
-	elseif event == "MOUSE_BUTTON_PRESSED" and arg == pubg.keyAlwayRun and pubg.isGKeyPressed == false then
-		OutputLogMessage("use AlwayRun\n")
-		PressKey(0x0d)
-		Sleep(400)
-		ReleaseKey(0x0d)
+		OutputLogMessage("weapon: ump9\n")
+	elseif event == "MOUSE_BUTTON_RELEASED" and arg == 5 and pubg.isOKeyPressed == false then
+		pubg.currentWeapon = "SCAR-L"
+		OutputLogMessage("weapon: scar-l\n")
 	end
-
+	
 	-- 鼠标左键被按下
 	if event == "MOUSE_BUTTON_PRESSED" and arg == 1 and pubg.currentWeapon ~= "NIL" then
-		repeat
-			if IsKeyLockOn ("capslock") then
-				if IsModifierPressed ("lshift") then
-					MoveMouseRelative(0, pubg.data[pubg.currentWeapon]["RELATIVE_X4"])
-					Sleep(pubg.data[pubg.currentWeapon]["SLEEP_X4"])
-				else
-					MoveMouseRelative(0, pubg.data[pubg.currentWeapon]["RELATIVE_X2"])
-					Sleep(pubg.data[pubg.currentWeapon]["SLEEP_X2"])
-				end
-			else
-				MoveMouseRelative(0, pubg.data[pubg.currentWeapon]["RELATIVE_X1"])
-				Sleep(pubg.data[pubg.currentWeapon]["SLEEP_X1"])
-			end
-		until not IsMouseButtonPressed(1)
+		if pubg.currentWeapon == "UMP9" then
+			pubg.onWeaponUMP9()
+		elseif pubg.currentWeapon == "SCAR-L" then
+			pubg.onWeaponScarL()
+		end
 	end
 end
+
 
 -------------------------------------------------------------------------------
 -- 驱动入口函数
@@ -103,3 +191,4 @@ end
 function OnEvent(event, arg)
 	pubg.onEvent(event, arg)
 end
+ClearLog()
